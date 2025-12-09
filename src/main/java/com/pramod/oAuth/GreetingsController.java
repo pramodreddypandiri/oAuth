3387c36e71543a2152c24705e51c1a18ff44d28f
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2025-12-08 19:21:25 -0500

    Init

--- src/main/java/com/pramod/oAuth/GreetingsController.java
+++ src/main/java/com/pramod/oAuth/GreetingsController.java
@@ -1,7 +1,9 @@
 package com.pramod.oAuth;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@GetMapping('/continue')
 public class GreetingsController {
 }
