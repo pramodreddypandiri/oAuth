63e4ca1c600bc343c1f32ad3690a64840f357986
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2025-12-08 19:16:15 -0500

    Init

---
+++ src/main/java/com/pramod/oAuth/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.pramod.oAuth;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated().anyRequest())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
